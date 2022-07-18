b69b31eef445447ba3347948f27b0b273a2d2166
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-07-18 09:01:40 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+
+const Header = () => {
+  return (
+    <header>
+      <img
+        src="https://bizmonthly.com/wp-content/uploads/2020/04/Amazon-logo-black-template.png"
+        className="header__logo"
+        alt=""
+      />
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      <nav className="header__nav">
+        <div className="header__nav-option">
+          <span className="header__nav-option_line1">Hello Guest</span>
+          <span className="header__nav-option_line2">Sign In</span>
+        </div>
+        <div className="header__nav-option">
+          <span className="header__nav-option_line1">Returns</span>
+          <span className="header__nav-option_line2">& Orders</span>
+        </div>
+        <div className="header__nav-option">
+          <span className="header__nav-option_line1">Your</span>
+          <span className="header__nav-option_line2">Prime</span>
+        </div>
+        <div className="header__nav-basket">
+          <ShoppingBasketIcon />
+          <span className="header__nav-option_line2 shopping__counter">0</span>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCp_V3488BBdpScdG06jVgU5kCOqDzdkns",
+  authDomain: "clone-5e950.firebaseapp.com",
+  projectId: "clone-5e950",
+  storageBucket: "clone-5e950.appspot.com",
+  messagingSenderId: "741181349531",
+  appId: "1:741181349531:web:af87d44131da3a2508f03e",
+};
